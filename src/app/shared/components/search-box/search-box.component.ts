765d760b47d905c6086c4fb269a9220fd5041f23
Diegucho7 <diegucho7t@gmail.com>
2023-06-19 23:43:15 -0500

    Ingreso de Input

--- src/app/shared/components/search-box/search-box.component.ts
+++ src/app/shared/components/search-box/search-box.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
     selector: 'gifs-search-box',
@@ -6,12 +6,26 @@ import { Component } from '@angular/core';
     <h5>Buscar:</h5>
     <input type="text"
     class="form-cpntrol"
-    placeholder="Buscar gifs..">
+    placeholder="Buscar gifs.."
+    (keyup.enter)="searchTag(  )"
+    #txtTagInput
+    >
+     
     `
 })
 
 export class SearchBoxComponent  {
+
+
+    @ViewChild('txtTagInput')
+    tagInput!: ElementRef<HTMLInputElement>;
+
     constructor() { }
 
-    ngOnInit() { }
+
+    searchTag(){
+        const newTag = this.tagInput.nativeElement.value;
+        console.log({newTag});
+    }
+
 }
