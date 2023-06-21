367e34c528024984bcd199503a15c3e7fb36a0c6
Diegucho7 <diegucho7t@gmail.com>
2023-06-20 23:00:59 -0500

    implementacion de TagHistory

--- src/app/shared/components/search-box/search-box.component.ts
+++ src/app/shared/components/search-box/search-box.component.ts
@@ -1,4 +1,5 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
+import { GifsService } from '../../../gifs/services/gifs.service';
 
 @Component({
     selector: 'gifs-search-box',
@@ -20,12 +21,13 @@ export class SearchBoxComponent  {
     @ViewChild('txtTagInput')
     tagInput!: ElementRef<HTMLInputElement>;
 
-    constructor() { }
+    constructor(private gifsService: GifsService) { }
 
 
     searchTag(){
         const newTag = this.tagInput.nativeElement.value;
-        console.log({newTag});
+       this.gifsService.searchTag(newTag);
+       this.tagInput.nativeElement.value = '';
     }
 
 }
