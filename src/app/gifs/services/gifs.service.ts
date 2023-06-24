5f144cd465e19278eb65b0e40bff117cf097b848
Diegucho7 <diegucho7t@gmail.com>
2023-06-23 23:12:20 -0500

    Implementacion de la aplicacion para buscar Gifs

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,13 +1,20 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+
+
+// const GIPHY_API_
+
 
 @Injectable({providedIn: 'root'})
 export class GifsService {
     
     private _tagsHistory: string[] = [];
 
+    private apiKey:     string = 'STruvUgEYEAbah4jIaaf0vlJvqgLvsn7';
+    private serviceUrl: string = 'http://api.giphy.com/v1/gifs';
 
     
-    constructor() { }
+    constructor( private http: HttpClient ) { }
     
     get tagsHistory(){
        return [...this._tagsHistory]; 
@@ -25,8 +32,20 @@ export class GifsService {
     searchTag( tag: string):void {
         if (tag.length === 0) return;
         this.organizeHistory(tag);
-        // this._tagsHistory.unshift(tag);
-        console.log(this.tagsHistory);
+
+        const params = new HttpParams()
+        .set('api_key', this.apiKey)
+        .set('limit', '10')
+        .set('q', tag)
+
+
+    this.http.get(`${this.serviceUrl}/search`,{params})
+    .subscribe( resp => {
+        console.log(resp);
+    })
+
+
+        
     }
 }
 
