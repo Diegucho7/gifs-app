3eb9bb315e20366071e537eb4e2cd6972a7673fc
Diegucho7 <diegucho7t@gmail.com>
2023-06-23 21:36:12 -0500

    Problema arreglado

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -9,23 +9,24 @@ export class GifsService {
     
     constructor() { }
     
-    get tagHistory(){
+    get tagsHistory(){
        return [...this._tagsHistory]; 
     }
 
     private organizeHistory(tag: string){
         tag = tag.toLowerCase();
         if(this._tagsHistory.includes(tag)){
-            this._tagsHistory = this._tagsHistory.filter((oldTag)=>oldTag !== tag)
+            this._tagsHistory = this._tagsHistory.filter((oldTag)=> oldTag !== tag)
         }
         this._tagsHistory.unshift(tag);
-        this._tagsHistory = this._tagsHistory.splice(0,10);
+        this._tagsHistory = this.tagsHistory.splice(0,10);
     }
 
     searchTag( tag: string):void {
         if (tag.length === 0) return;
+        this.organizeHistory(tag);
         // this._tagsHistory.unshift(tag);
-        console.log(this.tagHistory);
+        console.log(this.tagsHistory);
     }
 }
 
