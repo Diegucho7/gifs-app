5a56b193ebc99f62bc262926138ca282417e3b27
Diegucho7 <diegucho7t@gmail.com>
2023-06-27 22:24:55 -0500

    Seccion de Gifs realizado

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -12,12 +12,14 @@ export class GifsService {
     public gifList: Gif[] = [];
     
     private _tagsHistory: string[] = [];
-
     private apiKey:     string = 'STruvUgEYEAbah4jIaaf0vlJvqgLvsn7';
     private serviceUrl: string = 'http://api.giphy.com/v1/gifs';
 
     
-    constructor( private http: HttpClient ) { }
+    constructor( private http: HttpClient ) {
+        this.loadLocalStorage();
+        console.log('Gifs Service Ready');
+     }
     
     get tagsHistory(){
        return [...this._tagsHistory]; 
@@ -30,6 +32,19 @@ export class GifsService {
         }
         this._tagsHistory.unshift(tag);
         this._tagsHistory = this.tagsHistory.splice(0,10);
+        this.saveLocalStorage();
+    }
+
+
+    private saveLocalStorage():void{
+        localStorage.setItem('history',  JSON.stringify( this._tagsHistory ));
+    }
+
+    private loadLocalStorage():void{
+        if(!localStorage.getItem('history'))return;
+        this._tagsHistory = JSON.parse( localStorage.getItem('history')!);
+        if(this._tagsHistory.length === 0) return;
+        this.searchTag(this._tagsHistory[0]);
     }
 
     searchTag( tag: string):void {
@@ -45,7 +60,7 @@ export class GifsService {
     this.http.get<SearchResponse>(`${this.serviceUrl}/search`,{params})
     .subscribe( resp => {
         this.gifList = resp.data;
-        console.log({gifs: this.gifList});
+        // console.log({gifs: this.gifList});
     })
 
 
