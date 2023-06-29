0b58b1bf514c580995d76ab5b4585e4cd717f548
Diegucho7 <diegucho7t@gmail.com>
2023-06-28 22:56:40 -0500

    seguimiento de tarea para crear modulos

--- src/app/gifs/components/card-list/card-list.component.ts
+++ src/app/gifs/components/card-list/card-list.component.ts
@@ -1,11 +1,13 @@
 import { Component, Input } from '@angular/core';
 import { Gif } from '../../interfaces/gifs.interfaces';
+import { CardComponent } from '../../../shared/components/card/card.component';
 
 @Component({
   selector: 'gifs-card-list',
   templateUrl: './card-list.component.html',
 
 })
+
 export class CardListComponent {
 @Input()
 public gifs: Gif[] = [];
