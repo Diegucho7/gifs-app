fba15bb7f77817dd157742ad82d3b27224ddb401
Diegucho7 <diegucho7t@gmail.com>
2023-06-28 20:38:23 -0500

    prueba

--- src/app/shared/components/sidebar/sidebar.component.ts
+++ src/app/shared/components/sidebar/sidebar.component.ts
@@ -12,7 +12,9 @@ export class SidebarComponent {
 
 constructor( private gifsService: GifsService){}
 get tags(){
-  return this.gifsService.tagHistory;
+  return this.gifsService.tagsHistory;
 }
-
+searchTag(tag: string){
+  this.gifsService.searchTag(tag);
+}
 }
